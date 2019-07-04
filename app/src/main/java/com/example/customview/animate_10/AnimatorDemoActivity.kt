14c25909afbf896ccf9203f4ff0e16fe9a220a2c
wang <80002584@sf-express.com>
2019-07-04 15:03:31 +0800

    简化代码

--- app/src/main/java/com/example/customview/animate_10/AnimatorDemoActivity.kt
+++ app/src/main/java/com/example/customview/animate_10/AnimatorDemoActivity.kt
@@ -6,6 +6,7 @@ import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import com.example.customview.R
 import kotlinx.android.synthetic.main.activity_animator_demo.*
@@ -15,7 +16,8 @@ import kotlin.math.sin
 class AnimatorDemoActivity : AppCompatActivity() {
 
     private val list = ArrayList<ObjectAnimator>(8)
-    val set = AnimatorSet()
+    private val viewList = ArrayList<View>(8)
+    private val set = AnimatorSet()
     //夹角弧度值
     private var angle = Math.toRadians(22.5).toFloat()
     //移动距离
@@ -24,102 +26,70 @@ class AnimatorDemoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_animator_demo)
+        viewList.add(tvEast)
+        viewList.add(tvWest)
+        viewList.add(tvCenter)
+        viewList.add(tvSouth)
+        viewList.add(tvNorth)
         tvMaster.setOnClickListener {
             list.clear()
             addTranslations()
             addScales()
             addAlphas()
 
-
             set.duration = 1000
-            set.removeAllListeners()
             set.playTogether(list as Collection<Animator>?)
-            set.addListener(object : Animator.AnimatorListener {
-                override fun onAnimationRepeat(animation: Animator?) {
-                    println("repeat")
-                }
-
-                override fun onAnimationEnd(animation: Animator?) {
-                    println("end")
-                }
-
-                override fun onAnimationCancel(animation: Animator?) {
-                    println("cancel")
-                }
-
-                override fun onAnimationStart(animation: Animator?) {
-                    println("start")
-                }
-
-            })
             set.start()
         }
         initListener()
     }
 
     private fun initListener() {
-        tvEast.setOnClickListener { Toast.makeText(this, tvEast.text, Toast.LENGTH_SHORT).show() }
-        tvWest.setOnClickListener { Toast.makeText(this, tvWest.text, Toast.LENGTH_SHORT).show() }
-        tvCenter.setOnClickListener { Toast.makeText(this, tvCenter.text, Toast.LENGTH_SHORT).show() }
-        tvSouth.setOnClickListener { Toast.makeText(this, tvSouth.text, Toast.LENGTH_SHORT).show() }
-        tvNorth.setOnClickListener { Toast.makeText(this, tvNorth.text, Toast.LENGTH_SHORT).show() }
+        for (view in viewList) {
+            val v = view as TextView
+            v.setOnClickListener { Toast.makeText(this, v.text, Toast.LENGTH_SHORT).show() }
+        }
 
         btnCancel.setOnClickListener { set.cancel() }
         btnStart.setOnClickListener { set.reverse() }
-    }
 
-    private fun getTranslationX(tv: View, angle: Float): ObjectAnimator {
-        return ObjectAnimator.ofFloat(tv, "translationX", 0f, -radius * cos(angle))
-    }
+        set.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationRepeat(animation: Animator?) {
+                println("repeat")
+            }
 
-    private fun getTranslationY(tv: View, angle: Float): ObjectAnimator {
-        return ObjectAnimator.ofFloat(tv, "translationY", 0f, -radius * sin(angle))
+            override fun onAnimationEnd(animation: Animator?) {
+                println("end")
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+                println("cancel")
+            }
+
+            override fun onAnimationStart(animation: Animator?) {
+                println("start")
+            }
+        })
     }
 
     private fun addAlphas() {
-        list.add(ObjectAnimator.ofFloat(tvEast, "alpha", 0f, 1f))
-        list.add(ObjectAnimator.ofFloat(tvWest, "alpha", 0f, 1f))
-        list.add(ObjectAnimator.ofFloat(tvCenter, "alpha", 0f, 1f))
-        list.add(ObjectAnimator.ofFloat(tvSouth, "alpha", 0f, 1f))
-        list.add(ObjectAnimator.ofFloat(tvNorth, "alpha", 0f, 1f))
+        for (view in viewList) {
+            list.add(ObjectAnimator.ofFloat(view, "alpha", 0f, 1f))
+        }
     }
 
     private fun addScales() {
-        list.add(ObjectAnimator.ofFloat(tvEast, "scaleX", 0f, 1f))
-        list.add(ObjectAnimator.ofFloat(tvEast, "scaleY", 0f, 1f))
-        list.add(ObjectAnimator.ofFloat(tvWest, "scaleX", 0f, 1f))
-        list.add(ObjectAnimator.ofFloat(tvWest, "scaleY", 0f, 1f))
-        list.add(ObjectAnimator.ofFloat(tvCenter, "scaleX", 0f, 1f))
-        list.add(ObjectAnimator.ofFloat(tvCenter, "scaleY", 0f, 1f))
-        list.add(ObjectAnimator.ofFloat(tvSouth, "scaleX", 0f, 1f))
-        list.add(ObjectAnimator.ofFloat(tvSouth, "scaleY", 0f, 1f))
-        list.add(ObjectAnimator.ofFloat(tvNorth, "scaleX", 0f, 1f))
-        list.add(ObjectAnimator.ofFloat(tvNorth, "scaleY", 0f, 1f))
+        for (view in viewList) {
+            list.add(ObjectAnimator.ofFloat(view, "scaleX", 0f, 1f))
+            list.add(ObjectAnimator.ofFloat(view, "scaleY", 0f, 1f))
+        }
     }
 
     private fun addTranslations() {
-
-        val eastTrasnX = getTranslationX(tvEast, 0f)
-//        eastTrasnX.repeatCount = 1
-//        eastTrasnX.repeatMode = ValueAnimator.REVERSE
-        val westTrasnX = getTranslationX(tvWest, angle)
-        val westTrasnY = getTranslationY(tvWest, angle)
-        val centerTrasnX = getTranslationX(tvCenter, angle * 2)
-        val centerTrasnY = getTranslationY(tvCenter, angle * 2)
-
-        val southTrasnX = getTranslationX(tvSouth, angle * 3)
-        val southTrasnY = getTranslationY(tvSouth, angle * 3)
-        val northTrasnX = getTranslationX(tvNorth, angle * 4)
-        val northTrasnY = getTranslationY(tvNorth, angle * 4)
-        list.add(eastTrasnX)
-        list.add(westTrasnX)
-        list.add(westTrasnY)
-        list.add(centerTrasnX)
-        list.add(centerTrasnY)
-        list.add(southTrasnX)
-        list.add(southTrasnY)
-        list.add(northTrasnX)
-        list.add(northTrasnY)
+        for ((i, view) in viewList.withIndex()) {
+            list.add(ObjectAnimator.ofFloat(view, "translationX", 0f, -radius * cos(angle * i)))
+            list.add(ObjectAnimator.ofFloat(view, "translationY", 0f, -radius * sin(angle * i)))
+        }
     }
 
 }
